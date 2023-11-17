10711c1da43f79c4f6972217a9d5e40630dda2d6
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2023-11-17 15:49:08 +0100

    Finished branch

--- script.js
+++ script.js
@@ -17,48 +17,51 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection){
     let playerSelectionCaps = playerSelection.toUpperCase();
+    let final = "";
     switch(playerSelectionCaps){
         case "ROCK":
             if(computerSelection == "Paper"){
-                return "You lose!";
+                final = "You lose!";
             }
             else if(computerSelection == "Scissors"){
-                return "You win!";
+                final = "You win!";
             }
             else if (computerSelection == "Rock"){
-                return "It's a tie!";
+                final = "It's a tie!";
             }
 
         
         case "PAPER":
             if (computerSelection == "Rock"){
-                return "You win!";
+                final = "You win!";
             }
             else if(computerSelection == "Scissors"){
-                return "You lose!";
+                final = "You lose!";
             }
 
             else if(computerSelection == "Paper"){
-                return "It's a tie!";
+                final = "It's a tie!";
             }
         
         case "SCISSORS":
             if (computerSelection == "Rock"){
-                return "You lose!";
+                final = "You lose!";
             }
             else if(computerSelection == "Paper"){
-                return "You win!";
+                final = "You win!";
             }
             else if(computerSelection == "Scissors"){
-                return "It's a tie!";
+                final = "It's a tie!";
             }
     }
+    return final;
 }
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
 
+/*
 function game(){
     console.log("Playing 5 rounds");
     let winCounterPlayer = 0;
@@ -85,5 +88,70 @@ function game(){
         console.log("You tied!");
     }
 }
+*/ 
 
-game();
+let rb = document.querySelector("#rock");
+let pb = document.querySelector("#paper")
+let sb = document.querySelector("#scissors");
+rb.textContent = "Rock";
+
+let spanRes = document.querySelector("#resultbox");
+spanRes.setAttribute('style', 'flex: 1;');
+function checkScore(){
+    if (totalRounds == 5){
+        if (playerLost > playerWins){
+            alert("You won out of 5 rounds!");
+            totalRounds = 0;
+        }
+        else if(playerWins > playerLost){
+            alert("You lost out of 5 rounds");
+            totalRounds = 0;
+        }
+
+        else if (playerLost == playerWins){
+            alert("It was a tie in the end!");
+            totalRounds = 0;
+        }
+    }
+}
+
+let totalRounds = 0;
+let playerWins = 0;
+let playerLost = 0;
+
+rb.addEventListener("click", () => {
+    spanRes.textContent = playRound(rb.textContent,getComputerChoice());
+    totalRounds++;
+    if (spanRes.textContent == "You win!"){
+        playerWins++;
+    }
+    else if(spanRes.textContent == "You lose!"){
+        playerLost++;
+    }
+    checkScore();
+});
+pb.addEventListener("click", () => {
+    spanRes.textContent = playRound(pb.textContent,getComputerChoice());
+    totalRounds++;
+    if (spanRes.textContent == "You win!"){
+        playerWins++;
+    }
+    else if(spanRes.textContent == "You lose!"){
+        playerLost++;
+    }
+    checkScore();
+});
+sb.addEventListener("click", () => {
+    spanRes.textContent = playRound(sb.textContent,getComputerChoice());
+    totalRounds++;
+    if (spanRes.textContent == "You win!"){
+        playerWins++;
+    }
+    else if(spanRes.textContent == "You lose!"){
+        playerLost++;
+    }
+    checkScore();
+});
+
+
+
